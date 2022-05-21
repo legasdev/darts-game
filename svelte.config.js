c92f652c37c6c3687dba2c4ac07ea6d71e725f1c
Artem Stepanov <jekke-x@yandex.ru>
2022-05-21 14:59:59 +0300

    Первая попытка деплоя на ghPages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-github';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/darts-game'
+		}
 	}
 };
 
